ab0548e7acded22b7f82de4bedb799186af396c3
Filipe Barcellos <filipe.barcellos77@outlook.com>
2024-09-01 11:30:57 -0300

    Cubo da Velha Edu 0.1
    upd inicial

---
+++ Cubo da Velha versão educativa/Assets/SCRIPTS/Menu/MenuPrincipal.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Required for managing scenes
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public string MenuModos;
+    [SerializeField] private GameObject painelOpcoes;
+    [SerializeField] private GameObject painelCreditos;
+    [SerializeField] private GameObject Cubo;
+
+    public void OnJogar()
+    {
+        SceneManager.LoadScene(MenuModos);
+    }
+
+    public void Opcoes()
+    {
+        painelOpcoes.SetActive(true);
+    }
+
+    public void Creditos()
+    {
+        painelCreditos.SetActive(true);
+        Cubo.SetActive(false);
+    }
+
+    public void VoltarOpcoes()
+    {
+        painelOpcoes.SetActive(false);
+    }
+
+    public void VoltarCreditos()
+    {
+        painelCreditos.SetActive(false);
+        Cubo.SetActive(true);
+    }
+
+    public void Sair()
+    {
+    
+        Debug.Log("saiu do jogo!");
+        Application.Quit();
+    }
+}
